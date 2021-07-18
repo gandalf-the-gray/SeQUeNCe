eb3e95c7ad5a949f8b44eed778f7f133459ef3a8
Anubhav Srivastava <anubhav.14apr@gmail.com>
2021-07-18 20:51:20 +0530

    Comparative Analysis and initial graph plots

--- example/conti_code.py
+++ example/conti_code.py
@@ -20,6 +20,7 @@ def set_parameters(topology: Topology):
     MEMO_EXPIRE = 0
     MEMO_EFFICIENCY = 1
     MEMO_FIDELITY = 0.9349367588934053
+    #MEMO_FIDELITY = 0.99
     for node in topology.get_nodes_by_type("QuantumRouter"):
         node.memory_array.update_memory_params("frequency", MEMO_FREQ)
         node.memory_array.update_memory_params("coherence_time", MEMO_EXPIRE)
@@ -27,7 +28,8 @@ def set_parameters(topology: Topology):
         node.memory_array.update_memory_params("raw_fidelity", MEMO_FIDELITY)
 
     # set detector parameters
-    DETECTOR_EFFICIENCY = 0.9
+    #DETECTOR_EFFICIENCY = 0.9
+    DETECTOR_EFFICIENCY = 0.99
     DETECTOR_COUNT_RATE = 5e7
     DETECTOR_RESOLUTION = 100
     for node in topology.get_nodes_by_type("BSMNode"):
@@ -36,14 +38,17 @@ def set_parameters(topology: Topology):
         node.bsm.update_detectors_params("time_resolution", DETECTOR_RESOLUTION)
         
     # set entanglement swapping parameters
-    SWAP_SUCC_PROB = 0.90
-    SWAP_DEGRADATION = 0.99
+    #SWAP_SUCC_PROB = 0.90
+    SWAP_SUCC_PROB = 0.99
+    #SWAP_DEGRADATION = 0.99
+    SWAP_DEGRADATION = 1
     for node in topology.get_nodes_by_type("QuantumRouter"):
         node.network_manager.protocol_stack[1].set_swapping_success_rate(SWAP_SUCC_PROB)
         node.network_manager.protocol_stack[1].set_swapping_degradation(SWAP_DEGRADATION)
         
     # set quantum channel parameters
-    ATTENUATION = 1e-5
+    #ATTENUATION = 1e-5
+    ATTENUATION = 1e-10
     QC_FREQ = 1e11
     for qc in topology.qchannels:
         qc.attenuation = ATTENUATION
@@ -53,30 +58,62 @@ def set_parameters(topology: Topology):
 
 if __name__ == "__main__":
 
-    fidelityv = float(sys.argv[1])
+    fidelityIntermediate = float(sys.argv[1])
+    fidelityE2E = float(sys.argv[2])
+    isvirtual = str(sys.argv[3])
+    dest = str(sys.argv[4])
     
     set_parameters(network_topo)
 
-    node1 = "b"
-    node2 = "d"
-    nm = network_topo.nodes[node1].network_manager
-    nm.createvirtualrequest(node2, start_time=2e12, end_time=20e12, memory_size=1,target_fidelity=fidelityv )
+    if isvirtual == 'True':
+        node1 = "a"
+        node2 = "c"
+        nm = network_topo.nodes[node1].network_manager
+        nm.createvirtualrequest(node2, start_time=2e12, end_time=20e12, memory_size=1,target_fidelity=fidelityIntermediate )
 
-    node1 = "b"
-    node2 = "e"
-    nm = network_topo.nodes[node1].network_manager
-    nm.createvirtualrequest(node2, start_time=3e12, end_time=20e12, memory_size=1, target_fidelity=fidelityv)
+        #node1 = "d"
+        #node2 = "f"
+        #nm = network_topo.nodes[node1].network_manager
+        #nm.createvirtualrequest(node2, start_time=3e12, end_time=20e12, memory_size=1, target_fidelity=fidelityIntermediate)
 
-    # the start and end nodes may be edited as desired 
-    node1 = "g"
-    node2 = "i"
-    nm = network_topo.nodes[node1].network_manager
-    nm.createvirtualrequest(node2, start_time=2e12, end_time=20e12, memory_size=1, target_fidelity=fidelityv)
+        #the start and end nodes may be edited as desired 
+        #node1 = "g"
+        #node2 = "i"
+        #nm = network_topo.nodes[node1].network_manager
+        #nm.createvirtualrequest(node2, start_time=2e12, end_time=20e12, memory_size=1, target_fidelity=fidelityIntermediate)
 
 
-    tl.init()
-    tl.run()
-    print('tl.time= ',tl.time)
+        tl.init()
+        tl.run()
+
+        max_time = 0
+        max_time_fidelity = 0
+        for info in network_topo.nodes["a"].resource_manager.memory_manager:
+            if info.remote_node == 'c' and info.entangle_time > max_time:
+                max_time = info.entangle_time
+                max_time_fidelity = info.fidelity
+
+        #print('Fidelity obtained between a to c: ', max_time_fidelity)
+        """
+        max_time = 0
+        max_time_fidelity = 0
+        for info in network_topo.nodes["d"].resource_manager.memory_manager:
+            if info.remote_node == 'f' and info.entangle_time > max_time:
+                max_time = info.entangle_time
+                max_time_fidelity = info.fidelity
+
+        print('Fidelity obtained between d to f: ', max_time_fidelity)
+
+        max_time = 0
+        max_time_fidelity = 0
+        for info in network_topo.nodes["g"].resource_manager.memory_manager:
+            if info.remote_node == 'i' and info.entangle_time > max_time:
+                max_time = info.entangle_time
+                max_time_fidelity = info.fidelity
+
+        print('Fidelity obtained between g to i: ', max_time_fidelity)
+        """
+    #print('tl.time= ',tl.time)
     """
     print('tl.time= ',tl.time)
     print(node1, "memories")
@@ -88,13 +125,39 @@ if __name__ == "__main__":
     #print('--------------------------------------')
     #print("Second request starts")
 
+    
+    if isvirtual != 'True':
+        tl.init()
+
     tl.stop_time = 20e12#setting the simulation stop time, but ts not necessary that the simulation will stop at this, if all
                         #the simulation stops at the termination of last valid event, if valid events conitinue to be beyond this
                         #stop time then simulation stops at stop time.
+    req_start_time = 5e12
     nm2 = network_topo.nodes["a"].network_manager
-    nm2.request("i", start_time=5e12, end_time=20e12, memory_size=1, target_fidelity=0.6)
+    nm2.request(dest, start_time=req_start_time, end_time=20e12, memory_size=1, target_fidelity=fidelityE2E)
 
     tl.run()
+
+    
+    #Find the max entanglement time at A or I to find max timestep taken
+    max_time = 0
+    max_time_fidelity = 0
+    max_time_state = None
+    for info in network_topo.nodes["a"].resource_manager.memory_manager:
+        if info.remote_node == dest and info.entangle_time > max_time:
+            max_time = info.entangle_time
+            max_time_fidelity = info.fidelity
+            max_time_state = info.state
+
+            if info.entangle_time > 0  and info.entangle_time < req_start_time:
+                max_time = req_start_time
+
+
+    print(f"['{(max_time - req_start_time)*1e-12}','{round(max_time_fidelity, 3)}','{round(fidelityE2E, 3)}', '{max_time_state}']")
+    
+
+    #Along with this we'll print the fidelity too
+
     #print('tl.time= ',tl.time)
 
     """print("A memories")
@@ -150,7 +213,7 @@ if __name__ == "__main__":
     for info in network_topo.nodes["k"].resource_manager.memory_manager:
         print("{:6}\t{:15}\t{:9}\t{}\t{}".format(str(info.index), str(info.remote_node),
                                             str(info.fidelity), str(info.entangle_time * 1e-12),str(info.state))) 
-    """                                       
+                                           
 
     #Obtaining the physical graph
     nx_graph = network_topo.generate_nx_graph()
@@ -160,15 +223,13 @@ if __name__ == "__main__":
 
     #Obtaining the virtual graph
     virt_graph = network_topo.get_virtual_graph()
-    """for e in virt_graph.edges:
-        print("This is e---",e)"""
 
     network_topo.plot_graph(virt_graph)
 
 
 
 
-    """
+    
         for other in network_topo.nodes.keys():
 
             #Check if this is middle node then skip it
--- example/timing_test.py
+++ example/timing_test.py
@@ -4,6 +4,8 @@ import sys
 import time
 import subprocess
 import numpy as np
+import matplotlib.pyplot as plt
+import ast
 
 
 runtimes = []
@@ -26,12 +28,14 @@ if __name__ == "__main__":
     '''
 
     script = sys.argv[1]
+    #isvirtual = sys.argv[2]
     current_iter=0
     try:
         
-        fidelity=[x for x in np.arange(0.5,0.68,0.03)]
-        num_trials = len(fidelity)
-        
+        fidelityIntermediate=[x for x in np.arange(0.7,0.71,0.03)]
+        fidelityE2E = [x for x in np.arange(0.5,0.51,0.03)]
+        num_trials = len(fidelityE2E)*len(fidelityIntermediate)
+        destinations = ['c','d','e','f','g','h','i','j','k']
 
         
     except IndexError:
@@ -39,26 +43,53 @@ if __name__ == "__main__":
         
 
     @timeit_wrapper
-    def run(fidelity):
+    def run(fidelityInt, fidelityE2E, isvirtual ,dest):
         sys.stdout = open(os.devnull, 'w')
-        #sys.argv = ['0.7']
-        subprocess.call([sys.executable, 'conti_code.py', str(fidelity)])
-        #current_iter=current_iter+1
-        #exec(script)
-        #subprocess.call([script,'0.7'])
-        #exec(open(script).read())
+        retval = subprocess.check_output([sys.executable, 'conti_code.py', str(fidelityInt), str(fidelityE2E) , str(isvirtual), str(dest)]).decode(sys.stdout.encoding)
         sys.stdout = sys.__stdout__
+        return retval
 
     print("running timing test for {} with {} trials".format(script, num_trials))
 
-    for i in range(num_trials):
-        print("\trunning trial number {} ... ".format(i + 1), end='', flush=True)
-        run(fidelity[i])
-        print("ran in {}s".format(runtimes[-1]))
+    Physical_Ent_Time = []
+    Virtual_Ent_Time = []
+    distance_from_src = []
 
-    print("mean time: {}".format(stats.mean(runtimes)))
-    print("min time:  {}".format(min(runtimes)))
-    print("max time:  {}".format(max(runtimes)))
-    print("standard deviation: {}".format(stats.stdev(runtimes)))
+    #for i in range(num_trials):
+    for f_i in fidelityIntermediate:
+        for f_e2e in fidelityE2E:
+            for dest in destinations:
+                distance_from_src.append(destinations.index(dest))
+                print(f"Running for Destination: {dest}  intermediate fidelity value {round(f_i, 3)} and E2E fidelity value {round(f_e2e, 3)} \n", end='', flush=True)
+                print('Running for Physical')
+                retvalPhy = run(f_i, f_e2e, 'False' ,dest)
+                print('From retval ---- ', retvalPhy)
+                Physical_Ent_Time.append(float(ast.literal_eval(retvalPhy)[0]))
+                print(Physical_Ent_Time)
+               
+
+                print('Running for Virtual')
+                retvalVirt = run(f_i, f_e2e, 'True' ,dest)
+                Virtual_Ent_Time.append(float(ast.literal_eval(retvalVirt)[0]))
+                print(Virtual_Ent_Time)
+
+                print('From retval ---- ', retvalVirt)
+        #print("ran in {}s".format(runtimes[-1]))
+
+    #print("mean time: {}".format(stats.mean(runtimes)))
+    #print("min time:  {}".format(min(runtimes)))
+    #print("max time:  {}".format(max(runtimes)))
+    #print("standard deviation: {}".format(stats.stdev(runtimes)))
+
+fig, ax = plt.subplots()
+
+ax.plot(distance_from_src, Physical_Ent_Time, color = 'blue' ,label = r'Time for physical')
+ax.plot(distance_from_src, Virtual_Ent_Time, color = 'red', label = r'Time for virtual')
+
+ax.legend(loc = 'upper left')
+plt.xlabel('Distance From Source')
+plt.ylabel('Entanglement Time')
+plt.show()
+
 
 
